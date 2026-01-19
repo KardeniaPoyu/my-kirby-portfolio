65392c84347e1d8ce5a71a0104c3a4d3340ecb6c
KardeniaPoyu <yirongyiburong@gmail.com>
2026-01-20 00:07:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -176,7 +176,7 @@
   },
   {
     type: 'text',
-    content: "I have visited Japan multiple times and fallen in love with its unique blend of tradition and modernity. The vibrant city life, rich history, and warm people have left a lasting impression on me. So I hope to work in Japan and learn more about game development."
+    content: "I have visited Japan multiple times and fallen in love with its unique blend of tradition and modernity. The vibrant city life, rich history, and warm people have left a lasting impression on me. So I hope to work in Japan and learn more about game development there. Kirby is one of my favorite character!"
   },
   {
     type: 'image',
